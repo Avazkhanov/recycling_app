153cb788f5708142406ff7a3388c43beb45419d4
Dadakhan <147602032+Avazkhanov@users.noreply.github.com>
2025-04-20 14:06:07 +0500

    map key hided

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,13 +2,17 @@ import Flutter
 import UIKit
 import GoogleMaps
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDUsGcgAdeUmbI-5rJ_6YnL_zrSGc0w5ZU")
+
+    if let key = Bundle.main.infoDictionary?["GMSApiKey"] as? String {
+        GMSServices.provideAPIKey(key)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
